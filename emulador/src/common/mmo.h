0a8343d801f202cf11665550d812956f525ee89e
Jose Gois <jo.segois@hotmail.com>
2012-07-27 14:56:04 -0300

    comitando virgulas

--- emulador/src/common/mmo.h
+++ emulador/src/common/mmo.h
@@ -70,13 +70,13 @@
 #define MAX_MAP_PER_SERVER 1500 // Increased to allow creation of Instance Maps
 #define MAX_INVENTORY 100
 //Max number of characters per account. Note that changing this setting alone is not enough if the client is not hexed to support more characters as well.
-#define MAX_CHARS 9
+#define MAX_CHARS 12
 //Number of slots carded equipment can have. Never set to less than 4 as they are also used to keep the data of forged items/equipment. [Skotlex]
 //Note: The client seems unable to receive data for more than 4 slots due to all related packets having a fixed size.
 #define MAX_SLOTS 4
 //Max amount of a single stacked item
 #define MAX_AMOUNT 30000
-#define MAX_ZENY 1000000000
+#define MAX_ZENY 2000000000
 #define MAX_FAME 1000000000
 #define MAX_CART 100
 #define MAX_SKILL 3100
